e2bab4dbe9529964fac427022ef6b2fc60b7ec87
KowComical <xuanren1992@yahoo.com>
2023-10-08 15:33:10 +0800

    Automated commit

---
+++ upload.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+
+def main():
+    # Usage:
+    git_push("/data/xuanrenSong/CM_Power_Website")
+
+
+def git_push(repo_path, commit_message="Automated commit"):
+    try:
+        os.chdir(repo_path)
+        subprocess.run(['git', 'add', '--all'])
+        subprocess.run(['git', 'commit', '-m', commit_message])
+        subprocess.run(['git', 'push'])
+        print("Changes pushed successfully.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
