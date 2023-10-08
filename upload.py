70003f2341dc98eb6b245b876b21ba8c799cf3c7
KowComical <xuanren1992@yahoo.com>
2023-10-08 15:34:30 +0800

    Automated commit

--- upload.py
+++ upload.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 
+
 def main():
     # Usage:
     git_push("/data/xuanrenSong/CM_Power_Website")
@@ -9,10 +10,14 @@ def main():
 def git_push(repo_path, commit_message="Automated commit"):
     try:
         os.chdir(repo_path)
+
+        # Pull the latest changes from the remote repository
+        subprocess.run(['git', 'pull'])
+
         subprocess.run(['git', 'add', '--all'])
         subprocess.run(['git', 'commit', '-m', commit_message])
         subprocess.run(['git', 'push'])
-        print("Changes pushed successfully.")
+        print("Changes pulled and pushed successfully.")
     except Exception as e:
         print(f"Error: {e}")
 
