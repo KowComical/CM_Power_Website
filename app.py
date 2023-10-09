84e2c7bbf25a2e9bfb892f50b40cbc9336a6a381
KowComical <xuanren1992@yahoo.com>
2023-10-09 16:02:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def generate_grid_option(df_7mean, category_name):
             "yAxisIndex": idx,
             "data": date_min_values,
             "showSymbol": False,
-            "areaStyle": {"color": 'rgba(150, 150, 150, 0)', "opacity": 1},
+            "areaStyle": {"color": 'rgba(243,243,243)', "opacity": 1},
             "z": 100  # Higher value will be on top
         })
         
