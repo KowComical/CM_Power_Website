eeb5f76c6c2e2a1efe06d4b6af5093b8d8715c8e
KowComical <xuanren1992@yahoo.com>
2023-10-09 17:15:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,17 @@ st.set_page_config(layout="wide")
 with open('colors.txt', 'r') as file:
     COLORS = json.load(file)
 
+# Initialize st.session_state for the selected year
+if "selected_year" not in st.session_state:
+    st.session_state.selected_year = None
 
-# 主程序
 def main():
     st.sidebar.title('Power Generation Visualization')
 
     category_name = st.sidebar.selectbox(
         'Select Energy Type',
         ['total', 'coal', 'gas', 'oil', 'nuclear', 'hydro', 'wind', 'solar', 'other', 'fossil', 'renewables'],
-        index=0  # 将默认值设置为'power'
+        index=0
     )
 
     df, df_7mean = data_read()
@@ -32,16 +34,16 @@ def main():
     st_echarts(options=option,
                height=f"{PLOT_HEIGHT * ROWS_PER_GRID * 1.2}px")
 
-    # 获取颜色信息
     unique_years_all = df_7mean['year'].unique()
     colors_for_years = dict(zip(unique_years_all, get_line_colors(len(unique_years_all), category_name)))
 
     # 在侧边栏上显示图例
     st.sidebar.subheader("Legend: Year Colors")
     for year, color in colors_for_years.items():
+        if st.sidebar.button(f"{year}", key=f"year_{year}"):
+            st.session_state.selected_year = year if st.session_state.selected_year != year else None
         st.sidebar.markdown(f"<span style='color: {color};'>■</span> {year}", unsafe_allow_html=True)
 
-    # 使用 Streamlit 的下载按钮进行一键下载
     if category_name == 'total':
         csv_data = df[df['type'] != 'total'].to_csv(index=False)
     else:
@@ -53,30 +55,21 @@ def main():
         mime="text/csv"
     )
 
-
-# 生成 ECharts 配置的函数
 def generate_grid_option(df_7mean, category_name):
-    # 获取所有国家
     countries = df_7mean['country'].unique().tolist()
     num_countries = len(countries)
 
-    # 定义全局设置
-    # 动态计算行数和列数
     COLS = 4
-
-    # 计算所需的行数以容纳所有国家，每行4列
     ROWS = int(math.ceil(num_countries / COLS))
     WIDTH = 100 / COLS
     HEIGHT = 100 / ROWS
 
     ROWS_PER_GRID = math.ceil(len(countries) / COLS)
-    PLOT_HEIGHT = 250  # 根据需要进行调整
+    PLOT_HEIGHT = 250
 
-    # 调整间距
-    WIDTH_ADJUSTMENT = 0.8  # 增加或减少以调整水平间距
-    HEIGHT_ADJUSTMENT = 2.5  # 增加或减少以调整垂直间距
+    WIDTH_ADJUSTMENT = 0.8
+    HEIGHT_ADJUSTMENT = 2.5
 
-    # 格式化日期以用于 x 轴
     formatted_dates = df_7mean['date'].dt.strftime('%b-%d').drop_duplicates().tolist()
 
     option = {
@@ -89,14 +82,10 @@ def generate_grid_option(df_7mean, category_name):
         "series": []
     }
 
-    # 创建存储每年颜色的字典
     unique_years_all = df_7mean['year'].unique()
     colors_for_years = dict(zip(unique_years_all, get_line_colors(len(unique_years_all), category_name)))
 
-    # 创建图表的网格
     for idx, country in enumerate(countries):
-
-        # 创建网格并进行间距调整
         option["grid"].append({
             "top": f"{HEIGHT * (idx // COLS) + HEIGHT_ADJUSTMENT}%",
             "left": f"{WIDTH * (idx % COLS) + WIDTH_ADJUSTMENT}%",
@@ -105,12 +94,10 @@ def generate_grid_option(df_7mean, category_name):
             "containLabel": True
         })
 
-        # 过滤当前国家的数据
         country_data = df_7mean[df_7mean['country'] == country]
         min_val = float(round(country_data[country_data['type'] == category_name]['value'].min() * 0.95))
         max_val = float(round(country_data[country_data['type'] == category_name]['value'].max() * 1.05))
 
-        # 为网格创建 x 和 y 轴
         option["xAxis"].append({
             "gridIndex": idx,
             "type": "category",
@@ -124,15 +111,21 @@ def generate_grid_option(df_7mean, category_name):
             "max": max_val,
             "name": country,
             "nameTextStyle": {
-                "fontSize": 14,  # 根据需要进行调整
+                "fontSize": 14,
                 "fontWeight": "bold",
-                "padding": [0, 0, 0, 200]  # 如果需要，添加一些填充。[上，右，下，左]
+                "padding": [0, 0, 0, 200]
             }
         })
 
-        # 为每年生成系列数据
         unique_years = country_data['year'].unique()
         for year in unique_years:
+            opacity_val = 0.2
+            if st.session_state.selected_year:
+                if year == st.session_state.selected_year:
+                    opacity_val = 1
+                else:
+                    opacity_val = 0.1
+
             year_data = country_data[country_data['year'] == year]
             option["series"].append({
                 "name": f"{country} {year}",
@@ -142,9 +135,9 @@ def generate_grid_option(df_7mean, category_name):
                 "data": year_data[year_data['type'] == category_name]['value'].tolist(),
                 "itemStyle": {
                     "color": colors_for_years[year],
-                    "opacity": 0.2  # default opacity for all lines
+                    "opacity": opacity_val
                 },
-                "emphasis": {  # Add this block for emphasis styling
+                "emphasis": {
                     "lineStyle": {
                         "width": 4
                     },
@@ -152,13 +145,11 @@ def generate_grid_option(df_7mean, category_name):
                         "opacity": 1
                     }
                 },
-                "selectedMode": "single",  # Add this line to allow single line selection
+                "selectedMode": "single",
             })
 
     return option, ROWS_PER_GRID, PLOT_HEIGHT
 
-
-# 数据处理函数，通过 st.cache_data 进行缓存
 @st.cache_data
 def data_read():
     df = pd.read_csv('./data/data_for_download.csv')
@@ -169,34 +160,26 @@ def data_read():
 
     return df, df_7mean
 
-
-# 获取 CSV 下载链接的辅助函数
 def get_csv_download_link(df, filename="data.csv"):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">下载 CSV 文件</a>'
     return href
 
-
-# 获取线条颜色的辅助函数
 def get_line_colors(num_years, category_name):
-    base_grey = 200  # Starting from RGB(200, 200, 200), which is light grey
-    decrement = 15  # Decrease to darken the color, decrementing by 15 each year
+    base_grey = 200
+    decrement = 15
 
     colors = []
 
-    # Create a color palette for each year prior to the latest year
     for _ in range(num_years - 1):
         rgb_color = f'rgb({base_grey},{base_grey},{base_grey})'
         colors.append(rgb_color)
-        base_grey = max(50, base_grey - decrement)  # Don't go too dark, stop at RGB(50, 50, 50)
+        base_grey = max(50, base_grey - decrement)
 
-    # Add color for the latest year
-    colors.append(COLORS.get(category_name, 'rgb(220,20,60)'))  # Use color from COLORS dict or fallback to deep red
+    colors.append(COLORS.get(category_name, 'rgb(220,20,60)'))
 
     return colors
-
-
 
 if __name__ == '__main__':
     main()
