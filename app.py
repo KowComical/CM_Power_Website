630b3303946c6dce2c8a6a35aa477b07048f4abc
KowComical <xuanren1992@yahoo.com>
2023-10-09 16:29:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -122,18 +122,6 @@ def generate_grid_option(df_7mean, category_name):
         # Get min and max values for shadow area
         date_min_values, date_max_values = get_date_ranges(country_data, category_name)
         
-        # Add a dummy series with shadow color below the min series
-        option["series"].append({
-            "name": f"Dummy {country}",
-            "type": 'line',
-            "xAxisIndex": idx,
-            "yAxisIndex": idx,
-            "data": [value - 1 for value in date_min_values],  # This should be below your min data
-            "showSymbol": False,
-            "areaStyle": {"color": 'rgba(155, 255, 255, 1)'},
-            "z": 99  # Below the Min series
-        })
-        
         # Add the min series with white areaStyle to cover the dummy series
         option["series"].append({
             "name": f"Min {country}",
@@ -154,6 +142,7 @@ def generate_grid_option(df_7mean, category_name):
             "yAxisIndex": idx,
             "data": date_max_values,
             "showSymbol": False,
+            "areaStyle": {"color": 'rgba(150,150,150,1)'},  # White color
             "z": 98  # Below both Min and Dummy series
         })
 
