537539a869a798463b0136469167d7b279744c3d
KowComical <xuanren1992@yahoo.com>
2023-10-10 10:58:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,39 @@ import json
 
 show_pages_from_config()
 
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 # Set Streamlit Configuration
 st.set_page_config(layout="wide")
 
