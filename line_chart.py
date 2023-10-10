8b0b030e4af7dcb202a33bd8b384a97947ea622d
KowComical <xuanren1992@yahoo.com>
2023-10-11 04:15:19 +0800

    Update line_chart.py

--- line_chart.py
+++ line_chart.py
@@ -162,8 +162,7 @@ def generate_grid_option(df_7mean, category_name):
         for year in unique_years:
             year_data = country_data[country_data['year'] == year]
             option["series"].append({
-                # "name": f"{country} {year}", 
-                "name": str(year), 
+                "name": f"{year}", 
                 "type": "line",
                 "xAxisIndex": idx,
                 "yAxisIndex": idx,
