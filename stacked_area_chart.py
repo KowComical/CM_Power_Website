b19ebedfe60a5431cc9d76e82e688b6fdea97756
KowComical <xuanren1992@yahoo.com>
2023-10-11 18:24:53 +0800

    Update stacked_area_chart.py

--- stacked_area_chart.py
+++ stacked_area_chart.py
@@ -112,10 +112,10 @@ def generate_grid_area_option(df_7mean):
         country_dates = country_data['date'].dt.strftime('%Y-%m-%d').drop_duplicates().tolist()
 
         option["grid"].append({
-            "top": f"{HEIGHT * (idx // COLS) + HEIGHT_ADJUSTMENT + 5}%",
+            "top": f"{HEIGHT * (idx // COLS) + HEIGHT_ADJUSTMENT + 8}%",
             "left": f"{WIDTH * (idx % COLS) + WIDTH_ADJUSTMENT}%",
             "width": f"{WIDTH - 2 * WIDTH_ADJUSTMENT}%",
-            "height": f"{HEIGHT - 2 * HEIGHT_ADJUSTMENT}%",
+            "height": f"{HEIGHT - 4 * HEIGHT_ADJUSTMENT}%",
             "containLabel": True
         })
 
