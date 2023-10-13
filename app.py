b241e058422a7df882339dbbc787aef3246230cc
KowComical <xuanren1992@yahoo.com>
2023-10-13 20:50:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def main():
 
     with st.container():
       
-      add_logo("./data/logo.png", height=150)
+      st.sidebar.image("./data/logo.png")
       # Identify unique energy types and let users select one
       selected_energy = st.sidebar.selectbox(
           'Select Energy Type',
