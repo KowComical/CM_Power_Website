2a183b5532a40e0ce3fa1e04154619a9a2b78362
KowComical <xuanren1992@yahoo.com>
2023-10-20 03:29:54 +0800

    Update stacked_area_chart.py

--- stacked_area_chart.py
+++ stacked_area_chart.py
@@ -61,7 +61,7 @@ def main():
     'Nuclear': ['nuclear'],
     'Renewables': ['solar', 'wind', 'other', 'hydro']}
     
-    captions = [', '.join(items) for items in categories.values()]
+    captions = [', '.join(items.title()) for items in categories.values()]
     
     selected_category = st.sidebar.radio(
         'Sort Countries by Energy Category:',
