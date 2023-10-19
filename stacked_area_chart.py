a8c5dfabb7bf9ef9b61a1aa715cbc3fd6b646433
KowComical <xuanren1992@yahoo.com>
2023-10-20 03:21:24 +0800

    Update stacked_area_chart.py

--- stacked_area_chart.py
+++ stacked_area_chart.py
@@ -177,7 +177,7 @@ def generate_grid_area_option(df_7mean, selected_category):
         option["graphic"].append({
             "type": "text",
             "left": f"{WIDTH * (idx % COLS) + WIDTH_ADJUSTMENT + WIDTH/2}%",  # Centered horizontally
-            "top": f"{HEIGHT * (idx // COLS) + HEIGHT_ADJUSTMENT}%",  # At the top of the grid
+            "top": f"{HEIGHT * (idx // COLS) + HEIGHT_ADJUSTMENT + 2}%",  # At the top of the grid
             "z": 100,  # Place it above other elements
             "style": {
                 "text": f"{country} - {selected_category} {ratio_sum_str}",
