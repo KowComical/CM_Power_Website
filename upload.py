ef5371430da459fe3638659e3330f7ed2a4920d9
KowComical <xuanren1992@yahoo.com>
2023-10-26 11:45:51 +0800

    Automated commit

--- upload.py
+++ upload.py
@@ -18,7 +18,7 @@ country_list = ['Australia', 'Brazil', 'Chile', 'China',
                 'EU27&UK', 'France', 'Germany',
                 'India', 'Italy', 'Japan', 'Mexico', 'New Zealand',
                 'Russia', 'South Africa', 'Spain',
-                'Turkey', 'United Kingdom', 'United States', 'Bolivia', 'Bangladesh']
+                'Turkey', 'United Kingdom', 'United States', 'Bolivia', 'Bangladesh', 'Peru']
 
 categories = {
     'Fossil': ['coal', 'gas', 'oil'],
